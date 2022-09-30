ad576df87fc38a1dcabff7dbb4fd481ec944982d
stephan <stephangeorges980@gmial.com>
2022-09-30 17:06:13 +0200

    Quatrieme commit model

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ClientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $ville= $this->faker->city;
+        $pays= $this->faker->country;
+        return [
+            "nom"=> $this->faker->lastName,
+            "prenom"=> $this->faker->firstName,
+            "sexe"=> array_rand(["F", "H"], 1),
+            "dateNaissance"=> $this->faker->dateTimeBetween("1980-01-01", "2001-12-30"),
+            "lieuNaissance"=> "$pays, $ville",
+            "nationalite"=> $this->faker->country,
+            "pays"=> $pays,
+            "ville"=> $ville,
+            "adresse"=> $this->faker->address,
+            "telephone1"=> $this->faker->phoneNumber,
+            "telephone2"=> $this->faker->phoneNumber,
+            "pieceIdentite"=> array_rand(["CIN", "PASSEPORT", "PERMIS DE CONDUIRE"], 1),
+            "noPieceIdentite"=> $this->faker->creditCardNumber,
+
+        ];
+    }
+}
---
+++ database/seeders/PermissionTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("permissions")->insert([
+            ["nom"=> "ajouter un client"],
+            ["nom"=> "consulter un client"],
+            ["nom"=> "editer un client"],
+
+            ["nom"=> "ajouter une location"],
+            ["nom"=> "consulter une location"],
+            ["nom"=> "editer une location"],
+
+            ["nom"=> "ajouter un artcile"],
+            ["nom"=> "consulter un article"],
+            ["nom"=> "editer un article"]
+        ]);
+    }
+}
